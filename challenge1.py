faeac6b8c8d13e474bd15beebd17709fb23af7fc
Kyle Lam <kylelam2605@gmail.com>
2023-10-17 00:41:05 -0400

    modified chal 1 for column inputs

--- challenge1.py
+++ challenge1.py
@@ -6,19 +6,14 @@ from pulsar_metrics.metrics.drift import CustomDriftMetric
 @CustomDriftMetric
 def test_custom(current, reference, multiple=3, **kwargs):
     #Calculate z-scores of the feature for both the datasets
-    ref = pd.read_csv(reference)
-    cur = pd.read_csv(current)
 
-    metric_ref = ref[kwargs]
-    metric_cur = cur[kwargs]
+    std_dev_ref = np.std(reference)
+    mean_ref = np.mean(reference)
+    std_dev_cur = np.std(current)
+    mean_cur = np.mean(current)
 
-    std_dev_ref = np.std(metric_ref)
-    std_dev_cur = np.std(metric_cur)
-    mean_ref = np.mean(metric_ref)
-    mean_cur = np.mean(metric_cur)
-
-    z_score_ref = [(value - mean_ref) / std_dev_ref for value in metric_ref]
-    z_score_cur = [(value - mean_cur) / std_dev_cur for value in metric_cur]
+    z_score_ref = [(value - mean_ref) / std_dev_ref for value in reference]
+    z_score_cur = [(value - mean_cur) / std_dev_cur for value in current]
 
     max_z_score_ref = max(z_score_ref)
     min_z_score_ref = min(z_score_ref)
